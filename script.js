c0f57a1e2aaaf7f86288e5fc9dc61709dca810a9
as01 <adityasharan2001@gmail.com>
2023-07-22 14:34:04 +0530

    added a feature to mark priority and sorted the tasks according to their priorities

--- script.js
+++ script.js
@@ -2,54 +2,77 @@ var count = 1;
 var regex = /^\s*$/;
 var submitButton = document.getElementById("submitButton");
 var newItemTextBox = document.getElementById("newToDoItem");
+var newItemTextBox = document.getElementById("newToDoItem");
 var listOfToDoItems = document.getElementById("listOfToDoItems");
+var priorityList = document.getElementById("priorityList");
+var sortingFunction = prioritySortingfunction;
+
 listOfItems = [];
 
 submitButton.addEventListener("click", function(){
-    newItem(newItemTextBox.value);
+    newItem(newItemTextBox.value, priorityList.value);
 });
 
 newItemTextBox.addEventListener('keydown', (event) => {
     var name = event.key;
     if (name == 'Enter' )
     {
-        newItem(newItemTextBox.value);
+        newItem(newItemTextBox.value, priorityList.value);
     }
 }, false);
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => 
-    {
-        if (!response.ok) 
-        {
-            throw new Error("Network response was not OK");
-        }
-        return response.json();
-    })
-    .then((data) => 
-    {
-        // Process the received data
-        data.forEach(element => {
-            var ele = {id:element.id, text:element.title, done:element.completed};
-            var newItem = createListItem(ele);
-            count += 1;
-            listOfToDoItems.appendChild(newItem);
-            listOfItems.push(newItem);
-        });
-    })
-    .catch((error) => 
-    {
-        // Handle any errors that occurred during the fetch request
-        console.log("Error:", error.message);
-    });
 
+// API call to add a lot of tasks
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((response) => 
+//     {
+//         if (!response.ok) 
+//         {
+//             throw new Error("Network response was not OK");
+//         }
+//         return response.json();
+//     })
+//     .then((data) => 
+//     {
+//         // Process the received data
+//         data.forEach(element => {
+//             var ele = {id:element.id, text:element.title, done:element.completed};
+//             var newItem = createListItem(ele);
+//             count += 1;
+//             listOfToDoItems.appendChild(newItem);
+//             listOfItems.push(newItem);
+//         });
+//     })
+//     .catch((error) => 
+//     {
+//         // Handle any errors that occurred during the fetch request
+//         console.log("Error:", error.message);
+//     });
+
+priorityTable = {'High':1, 'Low': -1, 'Medium': 0};
+
+function prioritySortingfunction(a,b){
+    if(a.done != b.done)
+        return (a.done?1:-1);
+    else if(a.done && b.done)
+        return (a.id < b.id ? -1 : 1);
+    if(priorityTable[a.priority] == priorityTable[b.priority])
+        return (a.id < b.id ? -1 : 1);
+    return (priorityTable[a.priority] > priorityTable[b.priority] ? -1 : 1);
+}
+
+function dueDateSortingFunction(a,b){
+    return (a.dueDate > b.dueDate ? -1 : 1);
+}
 
 function render()
 {
     listOfToDoItems.innerHTML = "";
+    // console.log(listOfItems);
+    listOfItems.sort(sortingFunction);
     for(var i=0;i<listOfItems.length;++i)
     {
-        listOfToDoItems.appendChild(listOfItems[i]);
+        listOfToDoItems.appendChild(createListItem(listOfItems[i]));
     }
 }
 
@@ -66,7 +89,7 @@ function deleteItem(id)
     render();
 }
 
-function changeStatus(elem)
+function changeStatus(elem, id)
 {
     if(elem.style.backgroundColor == 'green')
     {
@@ -78,24 +101,34 @@ function changeStatus(elem)
         elem.style.backgroundColor = 'green';
         elem.innerHTML = "Done";
     }
+    for(var i=0;i<listOfItems.length;++i)
+    {
+        if(listOfItems[i].id == id)
+        {
+            listOfItems[i].done = !(listOfItems[i].done);
+        }
+    }
+    render();
 }
 
 function editTask(id)
 {
     var editButton = document.getElementById(id).getElementsByTagName('button')[2];
     var para = document.getElementById(id).getElementsByTagName('textarea')[0];
-
+    para.focus();
     if(editButton.innerHTML == 'Save')
     {
         editButton.style.backgroundColor = 'blue';
         editButton.innerHTML = 'Edit';
         para.readOnly = true;
+        // para.editing = true;
         return;
     }
 
     editButton.style.backgroundColor = 'grey';
     editButton.innerHTML = 'Save';
     para.readOnly = false;
+    // para.editing = false;
 }
 
 function createListItem(item)
@@ -103,7 +136,7 @@ function createListItem(item)
     var element = document.createElement('div');
     var para = document.createElement('textarea');
     var buttons = document.createElement('div');
-
+    var priorityList = document.createElement('select');
     var delButton = document.createElement('button');
     var doneButton = document.createElement('button');
     var editButton = document.createElement('button');
@@ -111,6 +144,7 @@ function createListItem(item)
     buttons.appendChild(delButton);
     buttons.appendChild(doneButton);
     buttons.appendChild(editButton);
+    buttons.appendChild(priorityList);
 
     element.appendChild(para);
     element.appendChild(buttons);
@@ -135,7 +169,7 @@ function createListItem(item)
     else
         doneButton.innerHTML = "Pending";
     doneButton.addEventListener("click", function(){
-        changeStatus(doneButton);
+        changeStatus(doneButton, item.id);
     });
 
     para.style.backgroundColor = '#87ACA3';
@@ -148,7 +182,10 @@ function createListItem(item)
     para.style.resize = 'none';
     para.innerHTML = item.text;
     para.readOnly = 'true';
+    para.editing = false;
     para.addEventListener('click', function(){
+        if(editButton.innerHTML == "Save")
+            return;
         editTask(element.id);
     });
 
@@ -162,18 +199,44 @@ function createListItem(item)
     buttons.style.width = "100%";
     buttons.classList.add('row');
 
+    var highOption = document.createElement('option');
+    highOption.value = 'High';
+    highOption.innerHTML = 'High';
+    var mediumOption = document.createElement('option');
+    mediumOption.value = 'Medium';
+    mediumOption.innerHTML = 'Medium';
+    var lowOption = document.createElement('option');
+    lowOption.value = 'Low';
+    lowOption.innerHTML = 'Low';
+
+    priorityList.appendChild(highOption);
+    priorityList.appendChild(mediumOption);
+    priorityList.appendChild(lowOption);
+    priorityList.value = item.priority;
+
+    if(priorityList.value == 'Low')
+    {
+        element.style.backgroundColor = 'lightblue';
+    }
+    else if(priorityList.value == 'High')
+    {
+        element.style.backgroundColor = 'red';
+    }
+    else
+    {
+        element.style.backgroundColor = '#87ACA3';
+    }
     return element;
 }
 
-function newItem(val)
+function newItem(val, priority)
 {
     if(regex.test(val))
         return;
-    var newItem = {id:count, text:val, done:false};
+    var newItem = {id:count, text:val, done:false, priority:priority};
     count += 1;
-    var newItem = createListItem(newItem);
-    listOfToDoItems.appendChild(newItem);
     listOfItems.push(newItem);
+    render();
     newItemTextBox.value = "";
 }
 
