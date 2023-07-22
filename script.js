2d5b78c1e6211b80e3dde6f7052fbb5d7240c47b
as01 <adityasharan2001@gmail.com>
2023-07-22 18:07:02 +0530

    added due date, filters, and categories partially

--- script.js
+++ script.js
@@ -7,32 +7,46 @@ var listOfToDoItems = document.getElementById("listOfToDoItems");
 var priorityList = document.getElementById("priorityList");
 var sortingFunction = prioritySortingfunction;
 
-listOfItems = [];
+var priorityTable = { High: 1, Low: -1, Medium: 0 };
 
-submitButton.addEventListener("click", function(){
+listOfItems = getlistOfItemsFromLocalStorage();
+render();
+
+submitButton.addEventListener("click", function () {
     newItem(newItemTextBox.value, priorityList.value);
 });
 
-newItemTextBox.addEventListener('keydown', (event) => {
-    var name = event.key;
-    if (name == 'Enter' )
-    {
-        newItem(newItemTextBox.value, priorityList.value);
-    }
-}, false);
+newItemTextBox.addEventListener(
+    "keydown",
+    (event) => {
+        var name = event.key;
+        if (name == "Enter") {
+            newItem(newItemTextBox.value, priorityList.value);
+        }
+    },
+    false
+);
 
+const sortTaksBySelection = document.getElementById("sortTasksBy");
+
+sortTaksBySelection.addEventListener("change", function () {
+    const selectedOption = sortTaksBySelection.value;
+    if (selectedOption == "Priority") sortingFunction = prioritySortingfunction;
+    else sortingFunction = dueDateSortingFunction;
+    render();
+});
 
 // API call to add a lot of tasks
 // fetch("https://jsonplaceholder.typicode.com/todos")
-//     .then((response) => 
+//     .then((response) =>
 //     {
-//         if (!response.ok) 
+//         if (!response.ok)
 //         {
 //             throw new Error("Network response was not OK");
 //         }
 //         return response.json();
 //     })
-//     .then((data) => 
+//     .then((data) =>
 //     {
 //         // Process the received data
 //         data.forEach(element => {
@@ -43,104 +57,104 @@ newItemTextBox.addEventListener('keydown', (event) => {
 //             listOfItems.push(newItem);
 //         });
 //     })
-//     .catch((error) => 
+//     .catch((error) =>
 //     {
 //         // Handle any errors that occurred during the fetch request
 //         console.log("Error:", error.message);
 //     });
 
-priorityTable = {'High':1, 'Low': -1, 'Medium': 0};
-
-function prioritySortingfunction(a,b){
-    if(a.done != b.done)
-        return (a.done?1:-1);
-    else if(a.done && b.done)
-        return (a.id < b.id ? -1 : 1);
-    if(priorityTable[a.priority] == priorityTable[b.priority])
-        return (a.id < b.id ? -1 : 1);
-    return (priorityTable[a.priority] > priorityTable[b.priority] ? -1 : 1);
+// Function to retrieve the listOfItems from Local Storage
+function getlistOfItemsFromLocalStorage() {
+    const storedList = localStorage.getItem("listOfItems");
+    return storedList ? JSON.parse(storedList) : [];
 }
 
-function dueDateSortingFunction(a,b){
-    return (a.dueDate > b.dueDate ? -1 : 1);
+// Function to save the listOfItems to Local Storage
+function saveListOfItemsToLocalStorage() {
+    localStorage.setItem("listOfItems", JSON.stringify(listOfItems));
 }
 
-function render()
-{
+function prioritySortingfunction(a, b) {
+    if (a.done != b.done) return a.done ? 1 : -1;
+    else if (a.done && b.done) return a.id < b.id ? -1 : 1;
+    if (priorityTable[a.priority] == priorityTable[b.priority])
+        return a.id < b.id ? -1 : 1;
+    return priorityTable[a.priority] > priorityTable[b.priority] ? -1 : 1;
+}
+
+function dueDateSortingFunction(a, b) {
+    if (a.done != b.done) return a.done ? 1 : -1;
+    return a.dueDate > b.dueDate ? -1 : 1;
+}
+
+function render() {
     listOfToDoItems.innerHTML = "";
-    // console.log(listOfItems);
     listOfItems.sort(sortingFunction);
-    for(var i=0;i<listOfItems.length;++i)
-    {
+    for (var i = 0; i < listOfItems.length; ++i) {
         listOfToDoItems.appendChild(createListItem(listOfItems[i]));
     }
+    saveListOfItemsToLocalStorage();
 }
 
-function deleteItem(id)
-{
+function deleteItem(id) {
     var index = 0;
-    for(var i=0;i<listOfItems.length;i++)
-    {
-        if(listOfItems[i].id == id)
-            index = i;
+    for (var i = 0; i < listOfItems.length; i++) {
+        if (listOfItems[i].id == id) index = i;
     }
     listOfItems.splice(index, 1);
     listOfToDoItems.innerHTML = "";
     render();
 }
 
-function changeStatus(elem, id)
-{
-    if(elem.style.backgroundColor == 'green')
-    {
-        elem.style.backgroundColor = 'red'
+function changeStatus(elem, id) {
+    if (elem.style.backgroundColor == "green") {
+        elem.style.backgroundColor = "red";
         elem.innerHTML = "Pending";
-    }
-    else
-    {
-        elem.style.backgroundColor = 'green';
+    } else {
+        elem.style.backgroundColor = "green";
         elem.innerHTML = "Done";
     }
-    for(var i=0;i<listOfItems.length;++i)
-    {
-        if(listOfItems[i].id == id)
-        {
-            listOfItems[i].done = !(listOfItems[i].done);
+    for (var i = 0; i < listOfItems.length; ++i) {
+        if (listOfItems[i].id == id) {
+            listOfItems[i].done = !listOfItems[i].done;
+            break;
         }
     }
     render();
 }
 
-function editTask(id)
-{
-    var editButton = document.getElementById(id).getElementsByTagName('button')[2];
-    var para = document.getElementById(id).getElementsByTagName('textarea')[0];
+function editTask(id) {
+    var editButton = document
+        .getElementById(id)
+        .getElementsByTagName("button")[2];
+    var para = document.getElementById(id).getElementsByTagName("textarea")[0];
     para.focus();
-    if(editButton.innerHTML == 'Save')
-    {
-        editButton.style.backgroundColor = 'blue';
-        editButton.innerHTML = 'Edit';
+    if (editButton.innerHTML == "Save") {
+        editButton.style.backgroundColor = "blue";
+        editButton.innerHTML = "Edit";
         para.readOnly = true;
         // para.editing = true;
         return;
     }
 
-    editButton.style.backgroundColor = 'grey';
-    editButton.innerHTML = 'Save';
+    editButton.style.backgroundColor = "grey";
+    editButton.innerHTML = "Save";
     para.readOnly = false;
     // para.editing = false;
+    render();
 }
 
-function createListItem(item)
-{
-    var element = document.createElement('div');
-    var para = document.createElement('textarea');
-    var buttons = document.createElement('div');
-    var priorityList = document.createElement('select');
-    var delButton = document.createElement('button');
-    var doneButton = document.createElement('button');
-    var editButton = document.createElement('button');
-    
+function createListItem(item) {
+    var element = document.createElement("div");
+    var para = document.createElement("textarea");
+    var buttons = document.createElement("div");
+    var priorityList = document.createElement("select");
+    var delButton = document.createElement("button");
+    var doneButton = document.createElement("button");
+    var editButton = document.createElement("button");
+	var dueDate = new Date(document.getElementById("dueDate").value);
+	// console.log(dueDate);
+
     buttons.appendChild(delButton);
     buttons.appendChild(doneButton);
     buttons.appendChild(editButton);
@@ -152,105 +166,99 @@ function createListItem(item)
     element.className = "listItem";
     element.id = "listItemNo" + String(item.id);
 
-    delButton.style.backgroundColor = 'black';
-    delButton.style.color = 'white';
+    delButton.style.backgroundColor = "black";
+    delButton.style.color = "white";
     delButton.innerHTML = "Delete";
-    delButton.addEventListener("click", function(){
+    delButton.addEventListener("click", function () {
         deleteItem(element.id);
     });
-    
-    if(item.done)
-        doneButton.style.backgroundColor = 'green';
-    else
-        doneButton.style.backgroundColor = 'red';
-    doneButton.style.color = 'white';
-    if(item.done)
-        doneButton.innerHTML = "Done";
-    else
-        doneButton.innerHTML = "Pending";
-    doneButton.addEventListener("click", function(){
+
+    if (item.done) doneButton.style.backgroundColor = "green";
+    else doneButton.style.backgroundColor = "red";
+    doneButton.style.color = "white";
+    if (item.done) doneButton.innerHTML = "Done";
+    else doneButton.innerHTML = "Pending";
+    doneButton.addEventListener("click", function () {
         changeStatus(doneButton, item.id);
     });
 
-    para.style.backgroundColor = '#87ACA3';
-    para.style.border = '0px';
-    para.style.display = 'initial';
-    para.style.width = '400px';
-    para.style.height = 'fit-content';
-    para.style.borderRadius = '5px';
-    para.style.border = '5px solid transparent';
-    para.style.resize = 'none';
+    para.style.backgroundColor = "#87ACA3";
+    para.style.border = "0px";
+    para.style.display = "initial";
+    para.style.width = "400px";
+    para.style.height = "fit-content";
+    para.style.borderRadius = "5px";
+    para.style.border = "5px solid transparent";
+    para.style.resize = "none";
     para.innerHTML = item.text;
-    para.readOnly = 'true';
+    para.readOnly = "true";
     para.editing = false;
-    para.addEventListener('click', function(){
-        if(editButton.innerHTML == "Save")
-            return;
+    para.addEventListener("click", function () {
+        if (editButton.innerHTML == "Save") return;
         editTask(element.id);
     });
 
-    editButton.style.backgroundColor = 'blue';
-    editButton.style.color = 'white';
+    editButton.style.backgroundColor = "blue";
+    editButton.style.color = "white";
     editButton.innerHTML = "Edit";
-    editButton.addEventListener("click", function(){
+    editButton.addEventListener("click", function () {
         editTask(element.id);
     });
 
     buttons.style.width = "100%";
-    buttons.classList.add('row');
+    buttons.classList.add("row");
 
-    var highOption = document.createElement('option');
-    highOption.value = 'High';
-    highOption.innerHTML = 'High';
-    var mediumOption = document.createElement('option');
-    mediumOption.value = 'Medium';
-    mediumOption.innerHTML = 'Medium';
-    var lowOption = document.createElement('option');
-    lowOption.value = 'Low';
-    lowOption.innerHTML = 'Low';
+    var highOption = document.createElement("option");
+    highOption.value = "High";
+    highOption.innerHTML = "High";
+    var mediumOption = document.createElement("option");
+    mediumOption.value = "Medium";
+    mediumOption.innerHTML = "Medium";
+    var lowOption = document.createElement("option");
+    lowOption.value = "Low";
+    lowOption.innerHTML = "Low";
 
     priorityList.appendChild(highOption);
     priorityList.appendChild(mediumOption);
     priorityList.appendChild(lowOption);
     priorityList.value = item.priority;
 
-    if(priorityList.value == 'Low')
-    {
-        element.style.backgroundColor = 'lightblue';
-    }
-    else if(priorityList.value == 'High')
-    {
-        element.style.backgroundColor = 'red';
-    }
-    else
-    {
-        element.style.backgroundColor = '#87ACA3';
+    if (priorityList.value == "Low") {
+        element.style.backgroundColor = "lightblue";
+    } else if (priorityList.value == "High") {
+        element.style.backgroundColor = "red";
+    } else {
+        element.style.backgroundColor = "#87ACA3";
     }
     return element;
 }
 
-function newItem(val, priority)
-{
-    if(regex.test(val))
-        return;
-    var newItem = {id:count, text:val, done:false, priority:priority};
+function newItem(val, priorityVal, dueDateVal) {
+    if (regex.test(val)) return;
+    var newItem = {
+        id: count,
+        text: val,
+        done: false,
+        priority: priorityVal,
+        dueDate: dueDateVal,
+    };
     count += 1;
     listOfItems.push(newItem);
     render();
     newItemTextBox.value = "";
 }
 
-// Assignment 3 - 
-// Todo list 
+// Assignment 3 -
+// Todo list
 // maintain a list of todo items  - keep a array in memory - [{ taskDetails : "do homework", id : 1}, { taskDetails : "sleep", id : 2}]
 // function to add to the array
 // function to remove a particular element in the array
 
-// UI - 
+// UI -
 // text box with save button
 // click event on the save button, and call the add array function
 // clear the text box
-// list of items to be displayed after click - from the array - RENDER FUNCTION to create UI of the list 
-// delete button next to each task in list 
+// list of items to be displayed after click - from the array - RENDER FUNCTION to create UI of the list
+// delete button next to each task in list
 // on click delete, pass ID of the task and remove from array
 // rerender the list after delete
